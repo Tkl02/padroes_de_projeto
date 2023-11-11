c7457ae26404a5f30b6bd984484c1f0ad798f851
Leonardo Faustino <84601712+Tkl02@users.noreply.github.com>
2023-11-11 16:34:52 -0300

    .

---
+++ game/main.py
@@ -0,0 +1,14 @@
+from pessoa import Pessoa
+
+# p1 = Pessoa('william', 21)
+# p1.eat('banana')
+# p1.eat('banana')
+# p1.para_comer()
+
+person_2 = Pessoa('amanda', 21)
+person_2.eat('farofa')
+person_2.eat('farofa')
+person_2.para_comer()
+person_2.eat('banana')
+person_2.para_comer()
+
